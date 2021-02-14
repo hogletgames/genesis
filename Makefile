38e24eca774e7a2bfdcc088561ee465603508496
Dmitry Shilnenkov <hogletgames@gmail.com>
2021-02-14 22:18:59 +0300

    Make: add linters

--- Makefile
+++ Makefile
@@ -9,6 +9,9 @@ BUILD_DIR           := build
 CMAKE_OPTIONS       := -DCMAKE_BUILD_TYPE=$(BUILD_TYPE) -DGE_STATIC=$(BUILD_STATIC) \
                        -DGE_DISABLE_ASSERTS=$(DISABLE_ASSERTS) -DGE_BUILD_EXAMPLES=$(BUILD_EXAMPLES)
 
+CLANG_FORMAT_BIN    ?= clang-format
+RUN_CLANG_TIDY_BIN  ?= run-clang-tidy
+
 # Building project
 .PHONY: all
 all: build_project
@@ -24,3 +27,13 @@ generate_makefiles:
 .PHONY: clean
 clean:
 	rm -rf build
+
+# Linters
+.PHONY: clang-format
+clang-format:
+	bash tools/clang_format.sh --clang-format-bin $(CLANG_FORMAT_BIN)
+
+.PHONY: clang-tidy
+clang-tidy: CMAKE_OPTIONS += -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+clang-tidy: generate_makefiles
+	$(RUN_CLANG_TIDY_BIN) -p $(BUILD_DIR)
---
+++ tools/clang_format.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+CLANG_FORMAT_BIN=clang-format
+EXIT_CODE=0
+FIX=false
+
+PATHS_TO_SRC="examples include src"
+SRC_FILES=$(find ${PATHS_TO_SRC} -name "*.h" -o -name "*.cpp")
+
+while [ -n "${1}" ]; do
+    case ${1} in
+    -f|--fix)
+        FIX=true
+        ;;
+    --clang-format-bin)
+        shift
+        CLANG_FORMAT_BIN=$1
+        ;;
+    *)
+        echo "Not supported arg: ${1}"
+        exit 1
+    esac
+    shift
+done
+
+for FILE in ${SRC_FILES}; do
+    echo "Checking: ${FILE}"
+    ${CLANG_FORMAT_BIN} --dry-run --Werror "${FILE}"
+
+    if [ $? -ne 0 ]; then
+        if ${FIX}; then
+            echo "Fixing: ${FILE}"
+            ${CLANG_FORMAT_BIN} --style=file -i "${FILE}"
+        else
+            EXIT_CODE=1
+        fi
+    fi
+done
+
+exit ${EXIT_CODE}
