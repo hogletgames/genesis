7e4426c5affc09b6dfc9d82003b1c6a21ce39327
Dmitry Shilnenkov <hogletgames@gmail.com>
2021-02-14 13:18:10 +0300

    genesis: core: implement export macro

---
+++ include/genesis/core/export.h
@@ -0,0 +1,58 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Dmitry Shilnenkov
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GENESIS_CORE_EXPORT_H_
+#define GENESIS_CORE_EXPORT_H_
+
+#ifndef GE_STATIC
+    #if GE_PLATFORM_WINDOWS
+        #define GE_API_EXPORT __declspec(dllexport)
+        #define GE_API_IMPORT __declspec(dllimport)
+    #else
+        // https://www.gnu.org/software/gnulib/manual/html_node/Exported-Symbols-of-Shared-Libraries.html
+        #if __GNUC__ >= 4
+            #define GE_API_EXPORT __attribute__((__visibility__("default")))
+            #define GE_API_IMPORT __attribute__((__visibility__("default")))
+        #endif // __GNUC__ >= 4
+    #endif     // GE_PLATFORM_WINDOWS
+#else
+    #define GE_API_EXPORT
+    #define GE_API_IMPORT
+#endif // GE_STATIC
+
+#ifdef GE_EXPORTS
+    #define GE_API GE_API_EXPORT
+#else
+    #define GE_API GE_API_IMPORT
+#endif // GE_EXPORTS
+
+#endif // GENESIS_CORE_EXPORT_H_
