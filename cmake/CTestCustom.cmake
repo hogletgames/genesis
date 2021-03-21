ceb8cb76106084a1cf5afb880eb48d8236abbfc3
Dmitry Shilnenkov <hogletgames@gmail.com>
2021-03-21 10:04:18 +0300

    cmake: add CTestsCustom.cmake to exclude permanent 3rd party tests

---
+++ cmake/CTestCustom.cmake
@@ -0,0 +1,5 @@
+set(CTEST_CUSTOM_TESTS_IGNORE
+    spirv-tools_expect_unittests
+    spirv-tools_spirv_test_framework_unittests
+    shaderc_expect_unittests
+    shaderc_glslc_test_framework_unittests)
