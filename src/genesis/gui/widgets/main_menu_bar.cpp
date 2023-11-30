806d240df33bb5931ecf7e1ba3ce71458aef8d44
Dmitry Shilnenkov <dmitry.shilnenkov@gmail.com>
2023-11-30 19:57:16 +0200

    ge: gui: widgets: implement Main Menu Bar

--- src/genesis/gui/widgets/main_menu_bar.cpp
+++ src/genesis/gui/widgets/main_menu_bar.cpp
@@ -1,7 +1,7 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2022, Dmitry Shilnenkov
+ * Copyright (c) 2023, Dmitry Shilnenkov
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,19 +30,16 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma once
+#include "main_menu_bar.h"
 
-#include <genesis/gui/widgets/button.h>
-#include <genesis/gui/widgets/checkbox.h>
-#include <genesis/gui/widgets/combo_box.h>
-#include <genesis/gui/widgets/image.h>
-#include <genesis/gui/widgets/input_text.h>
-#include <genesis/gui/widgets/main_menu_bar.h>
-#include <genesis/gui/widgets/separator.h>
-#include <genesis/gui/widgets/style_var.h>
-#include <genesis/gui/widgets/text.h>
-#include <genesis/gui/widgets/tree_node.h>
-#include <genesis/gui/widgets/value_editor.h>
-#include <genesis/gui/widgets/widget_node.h>
-#include <genesis/gui/widgets/widget_node_guard.h>
-#include <genesis/gui/widgets/window.h>
+#include <imgui.h>
+
+namespace GE::GUI {
+
+MainMenuBar::MainMenuBar()
+{
+    setBeginFunc(&ImGui::BeginMainMenuBar);
+    setEndFunc(&ImGui::EndMainMenuBar);
+}
+
+} // namespace GE::GUI
