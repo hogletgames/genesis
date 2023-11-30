ca488fd33616fcab27bd1d6c1fa2c47b58e41cd8
Dmitry Shilnenkov <dmitry.shilnenkov@gmail.com>
2023-11-30 19:48:30 +0200

    ge: gui: widgets: implement Input Text

--- src/genesis/gui/widgets/input_text.cpp
+++ src/genesis/gui/widgets/input_text.cpp
@@ -1,7 +1,7 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2022, Dmitry Shilnenkov
+ * Copyright (c) 2023, Dmitry Shilnenkov
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,18 +30,40 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma once
+#include "input_text.h"
+#include "private/types.h"
 
-#include <genesis/gui/widgets/button.h>
-#include <genesis/gui/widgets/checkbox.h>
-#include <genesis/gui/widgets/combo_box.h>
-#include <genesis/gui/widgets/image.h>
-#include <genesis/gui/widgets/input_text.h>
-#include <genesis/gui/widgets/separator.h>
-#include <genesis/gui/widgets/style_var.h>
-#include <genesis/gui/widgets/text.h>
-#include <genesis/gui/widgets/tree_node.h>
-#include <genesis/gui/widgets/value_editor.h>
-#include <genesis/gui/widgets/widget_node.h>
-#include <genesis/gui/widgets/widget_node_guard.h>
-#include <genesis/gui/widgets/window.h>
+#include "genesis/core/defer.h"
+
+#include <imgui.h>
+
+#include <vector>
+
+namespace GE::GUI {
+
+bool InputText::call(std::string_view label, std::string* output, Flags flags)
+{
+    static constexpr size_t BUFFER_SIZE{1024};
+    std::vector<char> buffer(BUFFER_SIZE, 0);
+    std::copy(output->begin(), output->end(), buffer.begin());
+
+    ImGui::PushID(output);
+    Defer defer{[] { ImGui::PopID(); }};
+
+    if (!ImGui::InputText(label.data(), buffer.data(), buffer.size(), flags)) {
+        return false;
+    }
+
+    *output = std::string(buffer.data());
+    return true;
+}
+
+void InputTextMultiline::call(std::string* text, const Vec2& widget_size)
+{
+    ImGui::PushID(text->c_str());
+    ImGui::InputTextMultiline("##text", text->data(), text->size(), toImVec2(widget_size),
+                              ImGuiInputTextFlags_ReadOnly);
+    ImGui::PopID();
+}
+
+} // namespace GE::GUI
