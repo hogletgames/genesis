e5d9661617bbf54023e2bcedb39c9122a878305a
Dmitry Shilnenkov <dmitry.shilnenkov@gmail.com>
2024-09-30 23:58:11 +0300

    [wip] tests: script: implement Method tests

---
+++ tests/script/data/MethodTests.cs
@@ -0,0 +1,133 @@
+namespace Ge.Tests {
+
+    public class MethodTest {
+        static public void ExistingMethod() {}
+
+        public sbyte ReturnSByte() {
+            return 42;
+        }
+
+        public sbyte SetSByte(sbyte value) {
+            return value;
+        }
+
+        public byte ReturnByte() {
+            return 42;
+        }
+
+        public byte SetByte(byte value) {
+            return value;
+        }
+
+        public short ReturnInt16()
+        {
+            return 42;
+        }
+
+        public short SetInt16(short value)
+        {
+            return value;
+        }
+
+        public ushort ReturnUInt16()
+        {
+            return 42;
+        }
+
+        public ushort SetUInt16(ushort value)
+        {
+            return value;
+        }
+
+        public int ReturnInt32()
+        {
+            return 42;
+        }
+
+        public int SetInt32(int value)
+        {
+            return value;
+        }
+
+        public uint ReturnUInt32()
+        {
+            return 42;
+        }
+
+        public uint SetUInt32(uint value)
+        {
+            return value;
+        }
+
+        public long ReturnInt64()
+        {
+            return 42;
+        }
+
+        public long SetInt64(long value)
+        {
+            return value;
+        }
+
+        public ulong ReturnUInt64()
+        {
+            return 42;
+        }
+
+        public ulong SetUInt64(ulong value)
+        {
+            return value;
+        }
+
+        public float ReturnFloat()
+        {
+            return 42f;
+        }
+
+        public float SetFloat(float value)
+        {
+            return value;
+        }
+
+        public double ReturnDouble()
+        {
+            return 42;
+        }
+
+        public double SetDouble(double value)
+        {
+            return value;
+        }
+
+        public bool ReturnBoolean()
+        {
+            return true;
+        }
+
+        public bool SetBoolean(bool value)
+        {
+            return value;
+        }
+
+        public char ReturnChar()
+        {
+            return '*'; // 42
+        }
+
+        public char SetChar(char value)
+        {
+            return value;
+        }
+
+        public string ReturnString()
+        {
+            return "42";
+        }
+
+        public string SetString(string value)
+        {
+            return value;
+        }
+    }
+
+}
