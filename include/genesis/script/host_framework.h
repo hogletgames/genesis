ea2085c134856892ab4da7ceb9aee226bcd04703
Dmitry Shilnenkov <dmitry.shilnenkov@gmail.com>
2025-04-20 19:06:40 +0300

    ge: script: implement Host Framework

--- include/genesis/script/host_framework.h
+++ include/genesis/script/host_framework.h
@@ -1,7 +1,7 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2021, Dmitry Shilnenkov
+ * Copyright (c) 2025, Dmitry Shilnenkov
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,15 +32,47 @@
 
 #pragma once
 
-#include <genesis/app.h>
-#include <genesis/assets.h>
-#include <genesis/core.h>
-#include <genesis/dll.h>
-#include <genesis/filesystem.h>
-#include <genesis/graphics.h>
-#include <genesis/gui.h>
-#include <genesis/math.h>
-#include <genesis/physics2d.h>
-#include <genesis/scene.h>
-#include <genesis/script.h>
-#include <genesis/window.h>
+#include <genesis/core/export.h>
+#include <genesis/core/function_traits.h>
+#include <genesis/core/memory.h>
+
+#include <string_view>
+
+#include <coreclr_delegates.h>
+
+namespace GE::Script {
+
+using LoadAssemblyAndGetFunctionPointerFn = FunctionType<load_assembly_and_get_function_pointer_fn>;
+using GetFunctionPointerFn = FunctionType<get_function_pointer_fn>;
+using LoadAssemblyFn = FunctionType<load_assembly_fn>;
+using LoadAssemblyBytesFn = FunctionType<load_assembly_bytes_fn>;
+
+class GE_API HostFramework
+{
+public:
+    static bool initialize(std::string_view runtime_config_path);
+    static void shutdown();
+
+    static LoadAssemblyAndGetFunctionPointerFn loadAssemblyAndGetFunctionPointerFn();
+    static GetFunctionPointerFn getFunctionPointerFn();
+    static LoadAssemblyFn loadAssemblyFn();
+    static LoadAssemblyBytesFn loadAssemblyBytesFn();
+
+    static bool isInitialized();
+
+private:
+    class Context;
+
+    HostFramework();
+    ~HostFramework();
+
+    static HostFramework* get()
+    {
+        static HostFramework instance;
+        return &instance;
+    }
+
+    Scoped<Context> m_context;
+};
+
+} // namespace GE::Script
