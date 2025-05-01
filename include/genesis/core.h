39f8c395b07ce7c30c1251df92c595ab6a24ccf3
Dmitry Shilnenkov <dmitry.shilnenkov@gmail.com>
2025-05-01 15:33:14 +0300

    ge: core: implement Function Traits

--- include/genesis/core.h
+++ include/genesis/core.h
@@ -41,6 +41,7 @@
 #include <genesis/core/exception.h>
 #include <genesis/core/export.h>
 #include <genesis/core/format.h>
+#include <genesis/core/function_traits.h>
 #include <genesis/core/interface.h>
 #include <genesis/core/log.h>
 #include <genesis/core/memory.h>
